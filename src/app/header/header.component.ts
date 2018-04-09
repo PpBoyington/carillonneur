f388a8e0379f3299796fb7f7966ebddc2bbbc281
Gregory <gregory.delfrate@gmail.com>
2018-04-09 20:45:02 +0200

    text header change

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -4,21 +4,17 @@ import {MatMenuTrigger} from '@angular/material';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
 
   @ViewChild(MatMenuTrigger) trigger: MatMenuTrigger;
-  public title = 'Carillonneur Edition';
 
   constructor() {
   }
 
   ngOnInit() {
 
-  }
-
-  someMethod() {
-    this.trigger.openMenu();
   }
 }
 
